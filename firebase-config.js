9f8e33033e27f719dea6dbf4e5cf6f4350960b75
jakotoleubek-crypto <jakotoleubek@gmail.com>
2026-09-22 22:53:36 +0500

    Add Firebase configuration to firebase-config.js
    Added Firebase configuration for the project.

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,9 @@
-/*
-  Ортақ онлайн альбом үшін Firebase Console ішінен web app ашып,
-  төмендегі false орнына өз конфигурацияңды қой.
-  Конфигурация болмаса, сайт осы құрылғыда (localStorage) жұмыс істей береді.
-
-  window.firebaseConfig = {
-    apiKey: "...",
-    authDomain: "...",
-    projectId: "...",
-    storageBucket: "...",
-    messagingSenderId: "...",
-    appId: "..."
-  };
-*/
+window.firebaseConfig = {
+  apiKey: "AIzaSyDCL60044uGMipcdamM85TQMiBrL7yTieM",
+  authDomain: "redgh-e438b.firebaseapp.com",
+  projectId: "redgh-e438b",
+  storageBucket: "redgh-e438b.firebasestorage.app",
+  messagingSenderId: "997441596280",
+  appId: "1:997441596280:web:a07244c97339ba9079768d",
+  measurementId: "G-RYBXLLB777"
+};
